a3603751f3911f4c09013492d0b4845ca59a7c71
Jason Zhou <jasonzhou903@qq.com>
2017-07-23 14:07:03 +0800

    update to OOP and fix responsive issues

--- demo.js
+++ demo.js
@@ -1,39 +1,52 @@
 (function () {
-  function updatePercent() {
-    const scrollTop = document.body.scrollTop
-    const docHeight = document.body.scrollHeight
-    const winHeight = window.innerHeight
-    const scrollPercent = (scrollTop) / (docHeight - winHeight)
-    const scrollPercentRounded = Math.round(scrollPercent * 100)
+  class App {
+    constructor() {
+      this.elements = {
+        percentLabel: document.getElementById('percent'),
+        rangeBar: document.getElementById('range-bar'),
+        mask: document.getElementById('mask'),
+      }
+    }
 
-    document.getElementById('percent').textContent = scrollPercentRounded
-    document.getElementById('range-bar').value = scrollPercent * 100
+    updatePercent() {
+      const { percentLabel, rangeBar  } = this.elements
+      const scrollTop = document.body.scrollTop
+      const docHeight = document.body.scrollHeight
+      const winHeight = window.innerHeight
+      const scrollPercent = (scrollTop) / (docHeight - winHeight)
+      const scrollPercentRounded = Math.round(scrollPercent * 100)
+
+      percentLabel.textContent = scrollPercentRounded
+      rangeBar.value = scrollPercent * 100
+    }
+
+    handleRangeBarChange(e) {
+      const rangeVal = e.target.value
+      const docHeight = document.body.scrollHeight
+      const winHeight = window.innerHeight
+      const scrollTop = (docHeight - winHeight) * rangeVal / 100
+
+      window.scroll(0, scrollTop)
+    }
+
+    hideMask() {
+      this.elements.mask.classList = 'hidden'
+    }
+
+    init() {
+      const { rangeBar } = this.elements
+
+      // Update the percent label and range bar when window scroll or resize
+      window.addEventListener('scroll', this.updatePercent.bind(this))
+      window.addEventListener('resize', this.updatePercent.bind(this))
+      // Update the window scroll position when range bar changes
+      rangeBar.addEventListener('input', this.handleRangeBarChange.bind(this))
+      // Hide the tip and mask when body clicked
+      document.body.addEventListener('click', this.hideMask.bind(this))
+    }
   }
 
-  function handleRangeBarChange(e) {
-    const rangeVal = e.target.value
-    const docHeight = document.body.scrollHeight
-    const winHeight = window.innerHeight
-    const scrollTop = (docHeight - winHeight) * rangeVal / 100
-
-    window.scroll(0, scrollTop)
-  }
-
-  function hideMask() {
-    document.getElementById('mask').classList = 'hidden'
-  }
-
-  function init() {
-    const rangeBar = document.getElementById('range-bar')
-
-    // Update the percent label and range bar when window scroll or resize
-    window.addEventListener('scroll', updatePercent)
-    window.addEventListener('resize', updatePercent)
-    // Update the window scroll position when range bar changes
-    rangeBar.addEventListener('input', handleRangeBarChange)
-    // Hide the tip and mask when body clicked
-    document.body.addEventListener('click', hideMask)
-  }
-
-  init()
+  const app = new App()
+  
+  app.init()
 })()
